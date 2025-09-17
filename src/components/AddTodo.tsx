0d4bb62b525c0e20b453fa151a852282baa58161
teruyoshi <teruyoshi.sakaya@gmail.com>
2025-09-17 18:02:22 +0900

    登録ボタンで Todo が登録されるようにした #5

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,8 +1,26 @@
-export default function AddTodo() {
+import type React from "react"
+import { useState } from "react"
+
+type AddTodoProps = {
+    addTodoHandler: (todo: string) => void
+}
+
+export default function AddTodo({addTodoHandler}: AddTodoProps) {
+    const [todo, setTodo] = useState<string>("")
+    const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTodo(e.target.value)
+    }
+
+    const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        addTodoHandler(todo)
+        setTodo("")
+    }
+
     return (
-      <div>
-        <input type="text" />
-        <button>登録</button>
-      </div>
+      <form onSubmit={onSubmitHandler}>
+        <input type="text" value={todo} onChange={onChangeHandler}/>
+        <button type="submit">登録</button>
+      </form>
     )
 }
